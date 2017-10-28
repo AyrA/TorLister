b2461f5c8172447840d15645195b60682b040f07
Kevin Gut <kevin.gut@bitm.sg>
2017-10-28 05:33:09 +0200

    Directory Signature type

---
+++ TorLister/DirectorySignature.cs
@@ -0,0 +1,18 @@
+﻿namespace TorLister
+{
+    public struct DirectorySignature
+    {
+        public string SignatureType;
+        public string Hash1;
+        public string Hash2;
+        public string Certificate;
+
+        public DirectorySignature(string[] Segments)
+        {
+            SignatureType = Segments[0];
+            Hash1 = Segments[1];
+            Hash2 = Segments[2];
+            Certificate = null;
+        }
+    }
+}
