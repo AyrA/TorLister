0bde95bc2ee168b8721d79de05a0bc123e35cb81
Kevin Gut <kevin.gut@bitm.sg>
2017-10-28 03:50:20 +0200

    Tools comments

--- TorLister/Tools.cs
+++ TorLister/Tools.cs
@@ -1,20 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
+﻿using System.Net;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace TorLister
 {
+    /// <summary>
+    /// Generic Tools
+    /// </summary>
     public static class Tools
     {
+        /// <summary>
+        /// Checks if the given string is a SHA1 hash
+        /// </summary>
+        /// <param name="Hash">SHA1 Hash</param>
+        /// <returns>true if valid Hash</returns>
         public static bool IsSHA1(string Hash)
         {
             return !string.IsNullOrEmpty(Hash) && Regex.IsMatch(Hash, @"[A-Fa-f\d]{40}");
         }
 
+        /// <summary>
+        /// Parses IP:Port into an IP Endpoint class
+        /// </summary>
+        /// <param name="S">IP Endpoint String</param>
+        /// <returns>IP Endpoint class</returns>
         public static IPEndPoint ParseEP(string S)
         {
             var IP = S.Substring(0, S.LastIndexOf(':')).Trim('[', ']');
@@ -22,6 +30,12 @@ namespace TorLister
             return new IPEndPoint(IPAddress.Parse(IP), int.Parse(Port));
         }
 
+        /// <summary>
+        /// Checks if the given string is a valid IPv4 Endpoint
+        /// </summary>
+        /// <param name="IpEntry">IPv4 Endpoint string</param>
+        /// <remarks>This will not make connection attempts</remarks>
+        /// <returns>true if valid IPv4 Endpoint</returns>
         public static bool IsIpV4Entry(string IpEntry)
         {
             IPAddress Addr = IPAddress.Any;
@@ -42,11 +56,22 @@ namespace TorLister
                 Addr.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork;
         }
 
+        /// <summary>
+        /// Checks if the given string is a Key Part
+        /// </summary>
+        /// <param name="KeyPart">Key Part string</param>
+        /// <returns>true if valid Key Part</returns>
         public static bool IsKeyPart(string KeyPart)
         {
             return !string.IsNullOrEmpty(KeyPart) && Regex.IsMatch(KeyPart, @"[A-F\d]{4}");
         }
 
+        /// <summary>
+        /// Checks if the given String is a valid Port Number
+        /// </summary>
+        /// <param name="Port">Port Number</param>
+        /// <remarks>This allows 0</remarks>
+        /// <returns>true if Port Number</returns>
         public static bool IsPort(string Port)
         {
             ushort p = 0;
