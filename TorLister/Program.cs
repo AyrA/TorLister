a625990d41b96069feb96c345401bbcd0d7acf14
Kevin Gut <kevin.gut@bitm.sg>
2017-10-28 15:51:00 +0200

    Add Random Linq Selector

--- TorLister/Program.cs
+++ TorLister/Program.cs
@@ -8,7 +8,10 @@ namespace TorLister
         static void Main(string[] args)
         {
             var Auth = Authorities.GetAuthorities();
-            var Consensus = new Directory(Auth.First().DownloadNodes());
+            
+            var Consensus = new Directory(Auth.Random().DownloadNodes());
+
+            Console.WriteLine(string.Join("\r\n",Consensus.TorNodes.Select(m => m.IP.ToString())));
 
             Console.WriteLine("#END");
             Console.ReadKey(true);
