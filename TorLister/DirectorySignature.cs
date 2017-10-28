beb1f8dac71fe3787b60e523149ef279f7bd791c
Kevin Gut <kevin.gut@bitm.sg>
2017-10-28 05:35:40 +0200

    Signature, not cert

--- TorLister/DirectorySignature.cs
+++ TorLister/DirectorySignature.cs
@@ -5,14 +5,14 @@
         public string SignatureType;
         public string Hash1;
         public string Hash2;
-        public string Certificate;
+        public string Signature;
 
         public DirectorySignature(string[] Segments)
         {
             SignatureType = Segments[0];
             Hash1 = Segments[1];
             Hash2 = Segments[2];
-            Certificate = null;
+            Signature = null;
         }
     }
 }
