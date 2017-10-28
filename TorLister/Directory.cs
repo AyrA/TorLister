764f7482e39548a2af6a613bcd9b0c60c5c9f6f1
Kevin Gut <kevin.gut@bitm.sg>
2017-10-28 04:44:31 +0200

    Directory Class

---
+++ TorLister/Directory.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TorLister
+{
+    public class Directory
+    {
+        public string VoteStatus
+        { get; private set; }
+
+        public int ConsensusMethod
+        { get; private set; }
+
+        public DateTime ValidAfter
+        { get; private set; }
+
+        public DateTime FreshUntil
+        { get; private set; }
+
+        public DateTime ValidUntil
+        { get; private set; }
+
+        public int[] VotingDelays
+        { get; private set; }
+
+        public string[] ClientVersions
+        { get; private set; }
+
+        public string[] ServerVersions
+        { get; private set; }
+
+        public string[] KnownFlags
+        { get; private set; }
+
+        public Dictionary<string, Version> RecommendedClientVersions
+        { get; private set; }
+
+        public Dictionary<string, Version> RecommendedRelayVersions
+        { get; private set; }
+
+        public Dictionary<string, Version> RequiredClientVersions
+        { get; private set; }
+
+        public Dictionary<string, Version> RequiredRelayVersions
+        { get; private set; }
+
+        public Dictionary<string, int> Params
+        { get; private set; }
+
+        public RandValue SharedRandPreviousValue
+        { get; private set; }
+
+        public RandValue SharedRandCurrentValue
+        { get; private set; }
+
+        /// <summary>
+        /// Parses a Network Consensus
+        /// </summary>
+        /// <param name="DirectorySource">Network Consensus</param>
+        public Directory(string DirectorySource)
+        {
+            using (var MS = new MemoryStream(Encoding.UTF8.GetBytes(DirectorySource.Replace("\n", "\r\n")), false))
+            {
+                using (var SR = new StreamReader(MS))
+                {
+                    //Verify Version
+                    if (SR.ReadLine() != "network-status-version 3 microdesc")
+                    {
+                        throw new ArgumentException("Directory Source doesn't looks like a Version 3 Microdescriptor");
+                    }
+                }
+            }
+        }
+    }
+}
--- TorLister/Program.cs
+++ TorLister/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace TorLister
 {
@@ -6,10 +7,17 @@ namespace TorLister
     {
         static void Main(string[] args)
         {
+            Version V = new Version("1-8,20,30,31,32,33,35,37,40-50");
+
+            Console.WriteLine(string.Join(",", V.Versions.Select(m => m.ToString())));
+            Console.WriteLine(V);
+
+            /*
             foreach (var Auth in Authorities.GetAuthorities())
             {
                 Console.WriteLine(Auth);
             }
+            //*/
             Console.WriteLine("#END");
             Console.ReadKey(true);
         }
